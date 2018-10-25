454e57e69e25efe273fa749fa7f52bab46554369
aivars.sazanovics <aivars.sazanovics>
2018-10-25 12:40:53 +0300

    Refactor container

---
+++ src/components/app/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../loading/Loading';
+import Error from '../error/Error';
+import Rates from '../rates/Rates';
+
+export class App extends Component {
+  handleClick = () => {
+    const { exchangeActions } = this.props;
+    exchangeActions.get();
+  };
+
+  render() {
+    const { error, exchange, loading } = this.props;
+
+    let template;
+    if (error) template = <Error error={error} />;
+    if (loading) template = <Loading />;
+    if (exchange.rates) template = <Rates rates={exchange.rates} />;
+
+    return (
+      <div>
+        <button type="button" onClick={() => this.handleClick()}>
+          Load exchange
+        </button>
+        {template}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  error: PropTypes.string,
+  exchange: PropTypes.shape({
+    date: PropTypes.string,
+    rates: PropTypes.object,
+    base: PropTypes.string
+  }),
+  loading: PropTypes.bool,
+  exchangeActions: PropTypes.object.isRequired
+};
+
+App.defaultProps = {
+  error: '',
+  exchange: {},
+  loading: false
+};
+
+export default App;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,53 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
 import * as exchangeActionCreator from '../actions/exchange';
-import Loading from '../components/loading/Loading';
-import Error from '../components/error/Error';
-import Rates from '../components/rates/Rates';
-
-export class App extends Component {
-  handleClick = () => {
-    const { exchangeActions } = this.props;
-    exchangeActions.get();
-  };
-
-  render() {
-    const { error, exchange, loading } = this.props;
-
-    let template;
-    if (error) template = <Error error={error} />;
-    if (loading) template = <Loading />;
-    if (exchange.rates) template = <Rates rates={exchange.rates} />;
-
-    return (
-      <div>
-        <button type="button" onClick={() => this.handleClick()}>
-          Load exchange
-        </button>
-        {template}
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  error: PropTypes.string,
-  exchange: PropTypes.shape({
-    date: PropTypes.string,
-    rates: PropTypes.object,
-    base: PropTypes.string
-  }),
-  loading: PropTypes.bool,
-  exchangeActions: PropTypes.object.isRequired
-};
-
-App.defaultProps = {
-  error: '',
-  exchange: {},
-  loading: false
-};
+import AppComponent from '../components/app/App';
 
 const mapStateToProps = state => {
   const { error, exchange, loading } = state.exchange;
@@ -65,4 +20,4 @@ const mapDispatchToProps = (dispatch, state) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(App);
+)(AppComponent);
